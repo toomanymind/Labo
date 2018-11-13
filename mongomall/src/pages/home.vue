7a3b3582632a86a24c4412497467f81a7a1a1a7e
toomanymind <851217479@qq.com>
2018-11-13 22:41:57 +0800

    mongomall files added

---
+++ mongomall/src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'home1 page.'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- mongomall/src/router/index.js
+++ mongomall/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/pages/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'home', component: home},
+    {path: '/helloworld', name: 'HelloWorld', component: HelloWorld}
   ]
 })
