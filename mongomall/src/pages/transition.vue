dc689c6b5b93f65c704eb1fbda38ae4f04af6041
toomanymind <851217479@qq.com>
2018-11-25 13:03:15 +0800

    some change

---
+++ mongomall/src/pages/transition.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="transition">
+    <h3>{{message}}</h3>
+    <card :person="person1"></card>
+  </div>
+</template>
+
+<script>
+  import card from '../components/card.vue'
+
+  export default {
+    components: {
+      card
+    },
+    data () {
+      return {
+        message: 'message',
+        code: 666666,
+        person1: {
+          name: 'Tom',
+          age: '23',
+          id: 't6adf6732831',
+          desc: 'some random guy',
+          hobbies: 'eat'
+        }
+      }
+    },
+    computed: {
+      cardStyle () {
+        return {
+          fontSize: `${this.fontSize}px`
+        }
+      }
+    },
+    methods: {
+      overflowHandler (payload) {
+        console.log(payload)
+      },
+      handler (e) {
+        console.log(e.currentTarget)
+      },
+      submit (e) {
+        console.log(e.key)
+      }
+    },
+    mounted () {
+      window['home'] = this
+    }
+  }
+</script>
+
+<style scoped>
+  .transition {
+    padding: 10px;
+  }
+</style>
