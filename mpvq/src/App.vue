0bfe34be17b4ed067edb7be0234a07db50242c8e
toomanymind <851217479@qq.com>
2018-12-28 21:30:15 +0800

    a

--- mpvq/src/App.vue
+++ mpvq/src/App.vue
@@ -11,14 +11,4 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-</style>
+<style></style>
